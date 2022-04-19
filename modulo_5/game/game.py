b6aa23028a49ffacee34ac38fead5e8eba01e902
Rafaelltm <rafaelltm10@hotmail.com>
2022-04-18 21:04:55 -0300

    criando a conexao com o banco

--- modulo_5/game/game.py
+++ modulo_5/game/game.py
@@ -1,10 +1,17 @@
 import state_handler as sh
 import globals
+import database
 
 import pyxel
 
 class Game:
     def __init__(self):
+        # testeCon = 
+        database.conecta_db()
+        # print(testeCon)
+        # sql = 'select * from arma'
+        # consulta = database.consultar_db(sql)
+        # print(consulta)
         pyxel.init(globals.WIDTH, globals.HEIGHT, title="Project Nuclear", fps=30)
         pyxel.mouse(True)
         pyxel.load("assets/assets.pyxres")
